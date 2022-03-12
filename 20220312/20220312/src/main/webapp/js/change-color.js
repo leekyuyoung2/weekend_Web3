b2c026249ba717f7290451b4c0688e8b91b2bf15
leekyuyoung2 <100980377+leekyuyoung2@users.noreply.github.com>
2022-03-12 13:00:30 +0900

    Add files via upload

---
+++ 20220312/20220312/src/main/webapp/js/change-color.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+document.addEventListener("DOMContentLoaded", function() {
+	var heading = document.querySelector('#heading');
+		heading.onclick = function() {
+		heading.style.color = "red";
+	}
+})
